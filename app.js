b438376c9d37eea6b00b7e221a1ce8cae97ee195
André Luiz <andregomesenator@gmail.com>
2020-03-18 16:41:43 -0300

    Salvando cadastro de cliente no Mongo.

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+require('./models/Usuarios');
+const Usuarios = mongoose.model('usuarios');
+
 const app = express();
 
-mongoose.connect('mongodb://localhost/andes', { useNewUrlParser: true, useUnifiedTopology: true }).then(() =>{
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/andes', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true }).then(() =>{
     console.log("Conexão com MongoDB realizada com sucesso.")
 }).catch((erro) =>{
-    console.log("Erro: conexão com o MongoDB não realizada com sucesso.")
+    console.log("Erro: conexão com o MongoDB não realizada com sucesso." + erro)
 });
 
 
 app.get("/usuarios", (req, res) => {
     res.send('Aprendendo a usar o node.');
+});
+
+app.post("/usuarios", (req, res) => {
+    var usuario = req.body;
+    Usuarios.create(req.body, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: "Erro: Usuário não cadatrado com sucesso!"
+        })
+
+        return res.json({
+            error: false,
+            message: "Usuário cadastrado com sucesso!"
+        })
+    });
 });
 
 app.listen(8080, () => {
