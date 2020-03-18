712d7329b6864e15d69b0023e35640ca5972782e
André Luiz <andregomesenator@gmail.com>
2020-03-18 17:25:49 -0300

    Atualização do app com 'Visualização, listagem e cadastro'.

--- app.js
+++ app.js
@@ -8,23 +8,44 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/andes', { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true }).then(() =>{
+mongoose.connect('mongodb://localhost/andes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.log("Conexão com MongoDB realizada com sucesso.")
-}).catch((erro) =>{
+}).catch((erro) => {
     console.log("Erro: conexão com o MongoDB não realizada com sucesso." + erro)
 });
 
-
+//Visualizar todos os cadastrados
 app.get("/usuarios", (req, res) => {
-    res.send('Aprendendo a usar o node.');
+    Usuarios.find({}).then((usuarios) => {
+        return res.json(usuarios);
+    }).catch((err) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum usuário encontrado."
+        });
+    });
 });
 
+//Listar um por um
+app.get("/usuarios/:id", (req, res) => {
+    Usuarios.findOne({ _id: req.params.id }).then((usuario) => {
+        return res.json(usuario);
+    }).catch((err) => {
+        return res.status(400).json({
+            error: true.value,
+            message: "Nenhum usuário encontrado!"
+        });
+    });
+});
+
+//Gravação no banco MongoDB
 app.post("/usuarios", (req, res) => {
     var usuario = req.body;
     Usuarios.create(req.body, (err) => {
-        if(err) return res.status(400).json({
+        if (err) return res.status(400).json({
             error: true,
             message: "Erro: Usuário não cadatrado com sucesso!"
         })
