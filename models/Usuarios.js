fe30463aa5cb97b237fadc4e5d94610ad3ed7afd
André Luiz <andregomesenator@gmail.com>
2020-03-19 07:10:04 -0300

    Atualização das rotas basicas do crud.

---
+++ models/Usuarios.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Usuarios = new mongoose.Schema({
+    nome:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+},
+{
+    timestamps: true,
+});
+mongoose.model('usuarios', Usuarios);
